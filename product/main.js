3510e668645da651d9937e4d22ec5bc034749b8f
shklan <ge2.julius@gmail.com>
2020-06-14 19:54:27 +0900

    js test8

--- product/main.js
+++ product/main.js
@@ -1,29 +1,31 @@
+window.onload = function () {
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(75, window.innterWidth / window.innerHeight, 0.1, 1000);
+    let renderer = new THREE.WebGLRenderer();
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innterWidth / window.innerHeight, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
+    let geometry = new THREE.BoxGeometry();
+    let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    let cube = new THREE.Mesh(geometry, material);
 
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-let cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-scene.add(cube);
+    camera.position.z = 5;
 
-camera.position.z = 5;
+    let animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        console.log("yeah");
+        renderer.render(scene, camera);
+    }
 
-let animate = function () {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    console.log("yeah");
-    renderer.render(scene, camera);
+    animate();
 }
 
-animate();
 
 
 
