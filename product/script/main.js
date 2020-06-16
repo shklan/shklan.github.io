17a1cc5ed28fd65e6559e78d2110613eae40c237
shklan <ge2.julius@gmail.com>
2020-06-16 23:43:02 +0900

    js test

--- product/script/main.js
+++ product/script/main.js
@@ -22,7 +22,7 @@ window.onload = function () {
     camera.position.z = 3;
 
     let animate = function () {
-        requestAnimationFrame(animate);
+//        requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
         renderer.render(scene, camera);
