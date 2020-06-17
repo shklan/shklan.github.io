e18732e0a1a47aacbd17f1a802605915233593ad
shklan <ge2.julius@gmail.com>
2020-06-17 12:18:36 +0900

    js test

--- product/script/main.js
+++ product/script/main.js
@@ -1,12 +1,12 @@
 `use strict`;
 
 window.onload = function () {
+    const base = document.createElement("canvas", {"width": window.innerWidth, "height": window.innerHeight});
+    document.getElementById("Canvas").appendChild(base);
+
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(45, window.innterWidth / window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    let renderer = new THREE.WebGLRenderer(base);
 
     let geometry = new THREE.BoxGeometry();
     let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
