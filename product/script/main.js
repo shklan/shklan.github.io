7b7b30291264e4070599b3254046042fba904e84
shklan <ge2.julius@gmail.com>
2020-08-13 22:14:53 +0900

    test three js

--- product/script/main.js
+++ product/script/main.js
@@ -4,8 +4,8 @@ window.onload = function () {
     const base = document.createElement("canvas");
     base.width = window.innerWidth;
     base.height = window.innerHeight;
-    
-    document.getElementById("Canvas").appendChild(base);
+
+    document.getElementsByClassName("Canvas")[0].appendChild(base);
 
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(45, window.innterWidth / window.innerHeight, 0.1, 1000);
