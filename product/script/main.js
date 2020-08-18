7d35ddf966a7d99b2a09f4d071e673cf801a1752
shklan <ge2.julius@gmail.com>
2020-08-18 19:31:00 +0900

    test

--- product/script/main.js
+++ product/script/main.js
@@ -2,15 +2,17 @@ import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/105/thre
 `use strict`;
 
 window.onload = function () {
-    const base = document.createElement("canvas");
-    base.width = window.innerWidth;
-    base.height = window.innerHeight;
+    // const base = document.createElement("canvas");
+    // base.width = window.innerWidth;
+    // base.height = window.innerHeight;
 
-    document.getElementsByClassName("Canvas")[0].appendChild(base);
+    // document.getElementsByClassName("Canvas")[0].appendChild(base);
 
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(45, window.innterWidth / window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer(base);
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSisze(window.innerWidth, window.innerHeight);
+    document.getElementsByClassName("Canvas")[0].appendChild(renderer.domElement);
 
     let geometry = new THREE.BoxGeometry();
     let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
